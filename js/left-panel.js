e0bf7af827137e2366ef9c66d7657eb5f73c3aeb
webdev983 <webdeveloper983@gmail.com>
2024-01-08 18:53:01 -0500

    !

--- js/left-panel.js
+++ js/left-panel.js
@@ -7,12 +7,12 @@ document.getElementById('log-open').addEventListener('click', function() {
   if (leftSidePanel.classList.contains('log-open')) {
     leftSidePanel.classList.remove('log-open');
     codeLog.setAttribute('hidden', '');
-    mainContent.style.marginLeft = '65px';
+    mainContent.style.marginLeft = '80px';
     myButton.classList.remove('b-selected'); // Remove the 'b-select' class
   } else {
     leftSidePanel.classList.add('log-open');
     codeLog.removeAttribute('hidden');
-    mainContent.style.marginLeft = '302px';
+    mainContent.style.marginLeft = '315px';
     myButton.classList.add('b-selected'); // Add the 'b-select' class
   }
 });
