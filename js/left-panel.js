e2d520b033cf981b79a07f877c3ce279b5e65dd6
webdev983 <webdeveloper983@gmail.com>
2024-01-08 17:25:36 -0500

    !

--- js/left-panel.js
+++ js/left-panel.js
@@ -1,18 +1,18 @@
-
-
 document.getElementById('log-open').addEventListener('click', function() {
   var leftSidePanel = document.querySelector('.left-side-panel');
   var codeLog = document.getElementById('code-log');
   var mainContent = document.getElementById('main-content');
+  var myButton = document.getElementById('myButton'); // Get the button element
 
   if (leftSidePanel.classList.contains('log-open')) {
     leftSidePanel.classList.remove('log-open');
     codeLog.setAttribute('hidden', '');
-    mainContent.style.marginLeft = '65px'; // Устанавливаем margin-left обратно на 100px
+    mainContent.style.marginLeft = '65px';
+    myButton.classList.remove('b-select'); // Remove the 'b-select' class
   } else {
     leftSidePanel.classList.add('log-open');
     codeLog.removeAttribute('hidden');
-    mainContent.style.marginLeft = '302px'; // Устанавливаем margin-left на 300px
+    mainContent.style.marginLeft = '302px';
+    myButton.classList.add('b-select'); // Add the 'b-select' class
   }
 });
-
