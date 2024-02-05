85cbaffcc57a50f77c240b20502d80cf693f7a06
webdev983 <webdeveloper983@gmail.com>
2024-02-05 17:47:41 -0500

    !

---
+++ attributes.groovy
@@ -0,0 +1,50 @@
+data-visual-title
+data-visual-border
+data-visual-element
+data-visual-draggable
+data-visual-insertBefore
+data-visual-appendChild
+data-visual-contenteditable
+data-visual-dublicate
+data-visual-defaultTextNode
+data-visual-checkbox
+
+data-builder-idParent
+data-builder-head
+
+data-product
+data-product-attribute-selection
+data-product-attribute-legend
+data-product-attribute-value
+data-product-info="color:blue;"
+
+data-builder-cart="heading"
+data-builder-cart="description-short"
+data-builder-cart="main-picture"
+data-builder-cart="price"
+
+data-builder-dependant="price"
+
+aria-live="polite"
+
+                srcset="
+                    320.webp 320w
+                    480.webp 480w, 
+                    640.webp 640w, 
+                    768.webp 768w, 
+                    960.webp 960w, 
+                    1280.webp 1280w, 
+                    1440.webp 1440w, 
+                    1600.webp 1600w, 
+                    1920.webp 1920w, 
+                    2048.webp 2048w, 
+                    2560.webp 2560w, 
+                    3200.webp 3200w, 
+                    5120.webp 5120w,
+                    "
+                    sizes="
+                    (max-width:767px) 100vw,
+                    (max-width:1024px) 70vw,
+                    (max-width:1920px) 50vw,
+                    400px
+                    ">
