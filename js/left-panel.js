c702941c076a97b676b3f13f7a7b8006057b249e
webdev983 <webdeveloper983@gmail.com>
2024-05-03 16:48:41 -0400

    !

--- js/left-panel.js
+++ js/left-panel.js
@@ -1,40 +1,34 @@
 document.getElementById('log-open').addEventListener('click', function() {
 
-  var asideElement = document.querySelector('.aside');
-  var leftElement = document.querySelector('.left');
-  var bottomButton = document.getElementById('bottom-buttons'); // Get the button element
-  var logOpenButton = document.getElementById('log-open'); 
-  var RefreshPageButton = document.getElementById('refresh-page'); 
-  var codeLog = document.getElementById('code-log');
-  var search_actions = document.getElementById('search-actions');
-  var refresh_page = document.getElementById('refresh-page');
+  const asideElement = document.querySelector('.aside');
+  const leftElement = document.querySelector('.left');
+  const bottomButton = document.getElementById('bottom-buttons'); 
+  const logOpenButton = document.getElementById('log-open'); 
+  const refreshPageButton = document.getElementById('refresh-page'); 
+  const codeLog = document.getElementById('code-log');
+  const searchActions = document.getElementById('search-actions');
+  const refreshPage = document.getElementById('refresh-page');
 
   if (logOpenButton.hasAttribute('data-log-opened')) {
 
     asideElement.style.width = '70px';
     leftElement.style.width = '60px';
-    refresh_page.setAttribute('hidden', '');
+    refreshPage.setAttribute('hidden', '');
     codeLog.setAttribute('hidden','');
-    search_actions.setAttribute('hidden','');
-    RefreshPageButton.setAttribute('hidden','');
+    searchActions.setAttribute('hidden','');
+    refreshPageButton.setAttribute('hidden','');
     bottomButton.removeAttribute('hidden');
     logOpenButton.removeAttribute('data-log-opened');
-
-
 
   } else {
     asideElement.style.width = '525px';
     leftElement.style.width = '515px';
-    refresh_page.removeAttribute('hidden');
+    refreshPage.removeAttribute('hidden');
     codeLog.removeAttribute('hidden');
-    search_actions.removeAttribute('hidden');
-    RefreshPageButton.removeAttribute('hidden');
+    searchActions.removeAttribute('hidden');
+    refreshPageButton.removeAttribute('hidden');
     bottomButton.setAttribute('hidden', '');
     logOpenButton.setAttribute('data-log-opened', '');
     logOpenButton.classList.add('b-selected'); 
-
-}
-
-
-
+  }
 });
