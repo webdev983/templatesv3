dac6a88ca17ab831ef358a41974351e13ae135d5
webdev983 <webdeveloper983@gmail.com>
2024-05-03 17:00:58 -0400

    !

--- js/left-panel.js
+++ js/left-panel.js
@@ -35,3 +35,9 @@ document.getElementById('log-open').addEventListener('click', function() {
     bottomButton.setAttribute('hidden', '');
   }
 });
+
+
+function reloadPage(event) {
+  event.preventDefault(); // Эта строка предотвращает выполнение действия по умолчанию для события (например, перезагрузка страницы)
+  window.location.reload(); // Эта строка перезагружает текущую страницу
+}
