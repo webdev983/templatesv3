53979efec78db6f573367d0ae22498e1f5520f13
webdev983 <webdeveloper983@gmail.com>
2024-05-22 17:11:41 -0400

    !

--- js/left-panel.js
+++ js/left-panel.js
@@ -56,8 +56,8 @@ document.getElementById('log-open').addEventListener('click', function() {
     codeLog.removeAttribute('hidden');
     searchActions.removeAttribute('hidden');
     refreshPageButton.removeAttribute('hidden');
-    asideElement.style.width = '525px';
-    leftElement.style.width = '515px';
+    asideElement.style.width = '625px';
+    leftElement.style.width = '615px';
   }
 });
 
